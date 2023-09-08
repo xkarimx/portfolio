06d112944f984e62446e21b22fc26a122dacb9c6
Karen <karen.perez.0395@gmail.com>
2023-09-08 11:17:27 -0300

    contact finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Home from './components/Home/Home'
+import Contact from './components/Contact/Contact'
 
 function App() {
   return (
+    <div>
     <Home />
+    <Contact />
+    </div>
   );
 }
 
